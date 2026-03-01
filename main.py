eead10d315c0068898d79ae81cd24e3c1bf348b1
Snigdha Sharma <snigdhanoor@gmail.com>
2026-02-28 20:42:06 -0800

    rewrite

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 import collections
+import dataclasses
 import itertools
 import pyinaturalist
 import termgraph
+
+
+@dataclasses.dataclass
+class ObservationSummary:
+    research_grade_observations: list[pyinaturalist.Observation]
+    research_grade_taxons: set[pyinaturalist.Taxon]
+    needs_id_observations: list[pyinaturalist.Observation]
+    needs_id_taxons: set[pyinaturalist.Taxon]
 
 
 def _species_name(name: str) -> str:
@@ -13,33 +22,17 @@ def _species_name(name: str) -> str:
 
 
 def print_lifetime_unique_species_chart(
-    species_observations: list[pyinaturalist.Observation],
+    summary: ObservationSummary,
 ):
-    rg_species_observations = [
-        obs for obs in species_observations if obs.quality_grade == "research"
-    ]
-    needs_id_species_observations = [
-        obs for obs in species_observations if obs.quality_grade == "needs_id"
-    ]
-
-    rg_species_taxons = set(
-        (_species_name(obs.taxon.name), obs.taxon.iconic_taxon_name)
-        for obs in rg_species_observations
-    )
-
-    needs_id_species_taxons = (
-        set(
-            (_species_name(obs.taxon.name), obs.taxon.iconic_taxon_name)
-            for obs in needs_id_species_observations
-        )
-        - rg_species_taxons
-    )
-
     rg_species_by_iconic_taxon = dict(
-        collections.Counter(taxon[1] for taxon in rg_species_taxons).most_common()
+        collections.Counter(
+            taxon.iconic_taxon_name for taxon in summary.research_grade_taxons
+        ).most_common()
     )
     needs_id_species_by_iconic_taxon = dict(
-        collections.Counter(taxon[1] for taxon in needs_id_species_taxons).most_common()
+        collections.Counter(
+            taxon.iconic_taxon_name for taxon in summary.needs_id_taxons
+        ).most_common()
     )
 
     termgraph.BarChart(
@@ -67,27 +60,23 @@ def print_lifetime_unique_species_chart(
 
 
 def print_day_unique_species_chart(
-    species_observations: list[pyinaturalist.Observation],
+    summary: ObservationSummary,
 ):
-    rg_species_observations = [
-        obs for obs in species_observations if obs.quality_grade == "research"
-    ]
-    needs_id_species_observations = [
-        obs for obs in species_observations if obs.quality_grade == "needs_id"
-    ]
-
     rg_species_by_day = {
-        day: set(obs.taxon.name for obs in group)
+        day: set((obs.taxon.name, obs.taxon.preferred_common_name) for obs in group)
         for day, group in itertools.groupby(
-            sorted(rg_species_observations, key=lambda obs: obs.observed_on.date()),
+            sorted(
+                summary.research_grade_observations,
+                key=lambda obs: obs.observed_on.date(),
+            ),
             key=lambda obs: obs.observed_on.date(),
         )
     }
     needs_id_species_by_day = {
-        day: set(obs.taxon.name for obs in group)
+        day: set((obs.taxon.name, obs.taxon.preferred_common_name) for obs in group)
         for day, group in itertools.groupby(
             sorted(
-                needs_id_species_observations, key=lambda obs: obs.observed_on.date()
+                summary.needs_id_observations, key=lambda obs: obs.observed_on.date()
             ),
             key=lambda obs: obs.observed_on.date(),
         )
@@ -131,20 +120,20 @@ def print_day_unique_species_chart(
         ),
     ).draw()
 
+    return {
+        entry[0]: needs_id_species_by_day.get(entry[0], set()) for entry in best_days
+    }
 
-def print_day_new_unique_species_chart(
-    species_observations: list[pyinaturalist.Observation],
-):
-    rg_species_observations = [
-        obs for obs in species_observations if obs.quality_grade == "research"
-    ]
 
+def print_day_new_unique_species_chart(summary: ObservationSummary):
     seen_species = set()
     new_species_by_day_and_iconic_taxon = collections.defaultdict(
         lambda: collections.Counter()
     )
     iconic_taxons = set()
-    for obs in sorted(rg_species_observations, key=lambda obs: obs.observed_on.date()):
+    for obs in sorted(
+        summary.research_grade_observations, key=lambda obs: obs.observed_on.date()
+    ):
         species_name = _species_name(obs.taxon.name)
         if species_name not in seen_species:
             seen_species.add(species_name)
@@ -186,19 +175,16 @@ def print_day_new_unique_species_chart(
     ).draw()
 
 
-def print_new_needs_id_species(species_observations: list[pyinaturalist.Observation]):
+def print_new_needs_id_species(summary: ObservationSummary):
     research_grade_species_taxons = set(
-        _species_name(obs.taxon.name)
-        for obs in species_observations
-        if obs.quality_grade == "research"
+        _species_name(taxon.name) for taxon in summary.research_grade_taxons
     )
     new_needs_id_species_observations = itertools.groupby(
         sorted(
             [
                 obs
-                for obs in species_observations
-                if obs.quality_grade == "needs_id"
-                and _species_name(obs.taxon.name) not in research_grade_species_taxons
+                for obs in summary.needs_id_observations
+                if _species_name(obs.taxon.name) not in research_grade_species_taxons
             ],
             key=lambda obs: obs.observed_on,
             reverse=True,
@@ -212,19 +198,33 @@ def print_new_needs_id_species(species_observations: list[pyinaturalist.Observat
     for species_name, group in new_needs_id_species_observations:
         observations = list(group)
         print(
-            f"{observations[0].taxon.emoji} {species_name}{f' ({observations[0].taxon.preferred_common_name})' if observations[0].taxon.preferred_common_name else ''}:"
+            f"{observations[0].taxon.emoji} {species_name}{f' ({observations[0].taxon.preferred_common_name})' if observations[0].taxon.preferred_common_name else ''}: "
+            + f"{', '.join(str(obs.observed_on.date()) for obs in observations)}"
         )
-        for obs in observations:
-            print(f"  {obs.observed_on.date()}")
-        print()
+    print()
 
 
-def main():
-    session = pyinaturalist.ClientSession(cache_file=".inat-cache.db")
-    response = pyinaturalist.get_observations(
-        user_id="sushain", session=session, page="all"
-    )
-    observations = pyinaturalist.Observation.from_json_list(response["results"])
+def print_best_days_new_needs_id_species(
+    best_days_needs_id_species: dict[str, set[str]],
+):
+    print("# Needs ID Species for Best Days")
+    print()
+    for day, species in best_days_needs_id_species.items():
+        if not species:
+            continue
+        print(
+            f"{day} ({len(species)}): "
+            + ", ".join(
+                f"{common_name if common_name else species_name}"
+                for species_name, common_name in species
+            )
+        )
+    print()
+
+
+def summarize_observations(
+    observations: list[pyinaturalist.Observation],
+) -> ObservationSummary:
 
     species_observations = [
         obs
@@ -232,10 +232,44 @@ def main():
         if obs.taxon.rank in ["species", "hybrid", "variety", "subspecies"]
     ]
 
-    print_lifetime_unique_species_chart(species_observations)
-    print_day_unique_species_chart(species_observations)
-    print_day_new_unique_species_chart(species_observations)
-    print_new_needs_id_species(species_observations)
+    research_grade_observations = [
+        obs for obs in species_observations if obs.quality_grade == "research"
+    ]
+    needs_id_observations = [
+        obs for obs in species_observations if obs.quality_grade == "needs_id"
+    ]
+
+    research_grade_taxons = {
+        _species_name(obs.taxon.name): obs.taxon for obs in research_grade_observations
+    }
+    needs_id_taxons = {
+        _species_name(obs.taxon.name): obs.taxon
+        for obs in needs_id_observations
+        if _species_name(obs.taxon.name) not in research_grade_taxons
+    }
+
+    return ObservationSummary(
+        research_grade_observations=research_grade_observations,
+        research_grade_taxons=list(research_grade_taxons.values()),
+        needs_id_observations=needs_id_observations,
+        needs_id_taxons=list(needs_id_taxons.values()),
+    )
+
+
+def main():
+    session = pyinaturalist.ClientSession()
+    response = pyinaturalist.get_observations(
+        user_id="sushain", session=session, page="all"
+    )
+    observations = pyinaturalist.Observation.from_json_list(response["results"])
+
+    summary = summarize_observations(observations)
+
+    print_lifetime_unique_species_chart(summary)
+    best_days_needs_id_species = print_day_unique_species_chart(summary)
+    print_day_new_unique_species_chart(summary)
+    print_new_needs_id_species(summary)
+    print_best_days_new_needs_id_species(best_days_needs_id_species)
 
 
 if __name__ == "__main__":
